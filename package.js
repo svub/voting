b54e2ff654b0136e5ff054d93e9757b010c5f989
svub <me@svenbuschbeck.net>
2014-06-18 16:27:07 +0300

    Voting's own package. Added ClientVoting.hookupTemplate for easily doing standard vote-up vote-down thingy.

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  summary: "Provides voting/rating/counting functionality for one item of a collection rating/counting an item of the same or different collection."
+});
+
+Package.on_use(function (api, where) {
+  if(api.export) { api.export('voting'); }
+  
+  common = ['client', 'server'];
+
+  
+  api.use(['underscore', 'coffeescript', 'underscore-string-latest', 'meteor', 'templating', 'ejson', 'mongo-livedata', 'deps'], common);
+  api.use(['minimongo', 'less'], 'client');  
+
+  api.add_files('voting.coffee', common);
+
+});
